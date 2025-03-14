0230cd787245c51d0cbf118133887defeb15d922
Arghya Chakrabarty <arghya2801@gmail.com>
2025-03-14 11:47:09 +0000

    Added the routes for the sensors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import sensorRoutes from "./routes/sensor.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Routes
+app.use('/api', sensorRoutes);
+
+app.get('/', (req, res) => {
+	res.json({ message: "Sensors" });
+  });
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
