7dc9b75a516fe6748c21351d3371c3ba8e0fc75e
Arghya <arghya2801@gmail.com>
2025-04-17 01:22:57 +0530

    Added and allowed CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import sensorRoutes from "./routes/sensor.route.js";
 import csvRoutes from "./routes/csv.route.js";
@@ -8,6 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 // Routes
 app.use('/api', sensorRoutes);
